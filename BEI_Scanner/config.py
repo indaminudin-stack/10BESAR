092e1b25863bfb068946a5147dfea8d151e4a5ce
indaminudin-stack <indaminudin@gmail.com>
2026-07-05 18:48:46 +0700

    Delete BEI_Scanner directory

--- BEI_Scanner/config.py
+++
@@ -1,2 +0,0 @@
-import pandas as pd
-STOCKS = pd.read_csv("stocks.csv") ["Stock"].tolist()
--- BEI_Scanner/downloader.py
+++
@@ -1,6 +0,0 @@
-import yfinance as yf
-def download_stock(stock):
-    df = yf.download( stock, period="1mo", progress=False, auto_adjust=False
-    )
-    df.columns = df.columns.get_level_values(0)
-    return df
